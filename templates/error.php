b2dcb6ec63f89869d0c49c0a18959c9ab1dc32e9
nobud <nobud@yandex.ru>
2018-09-28 17:13:07 +0300

    опечатка

--- templates/error.php
+++ templates/error.php
@@ -1,6 +1,6 @@
 <h2 class="content__main-heading">Ошибка</h2>
 <table class="tasks">
-  <tr class="tasks__item task"
+  <tr class="tasks__item task">
     <td>
       <span class="error"><?= $error; ?></span>
     </td>
